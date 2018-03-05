a3c875c2cd663f35e3be832a1ccaf09ab9a1e026
Maximal <maxim@infofree.ru>
2018-03-05 22:34:09 +0500

    add static route and static help page

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.send({
-    name: 'Andrew',
+    name: 'Max',
     likes: [
       'Biking',
       'Cities',
@@ -25,4 +26,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+})
