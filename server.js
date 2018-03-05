4180656ac5f9f9d104db056d9b1f9e8a4864b9f1
Maximal <maxim@infofree.ru>
2018-03-05 23:27:54 +0500

    add UpperCase helper for title in home.hbs

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 })
 
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
